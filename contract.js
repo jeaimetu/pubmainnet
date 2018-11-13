0424844f9e2b946b136fcc38a36b18f66ddab4d6
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-11-13 13:16:34 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -53,11 +53,11 @@ var testers = [
 	];
 
 config = {
-  //chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", // 32 byte (64 char) hex string
-  chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
+  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", // 32 byte (64 char) hex string
+  //chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
   keyProvider: process.env.key, // WIF string or array of keys..
-  //httpEndpoint: 'https://mainnet.eoscalgary.io',
-  httpEndpoint:	"http://jungle.cryptolions.io:18888",
+  httpEndpoint: 'https://mainnet.eoscalgary.io',
+  //httpEndpoint:	"http://jungle.cryptolions.io:18888",
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
@@ -66,7 +66,7 @@ config = {
 
 eos = Eos(config);
 
-const contractOwner = "eoscafekorea";
+const contractOwner = "publytokenio";
 
 async function getInternalBalance(account){
 	
