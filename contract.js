bfb7c632b32332e2251c8f055f7b5eb33be7a70f
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-04-04 14:36:18 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -281,7 +281,7 @@ exports.stakelist = async function(callback){
 	//getData().then(data => callback(data));	
 	MongoClient.connect(url, function(err, db) {
 		var dbo = db.db("heroku_dx6phtwp");
-		dbo.collection("board").find().toArray(function(err, docs) {
+		dbo.collection("board").find().sort( { amount : -1}).toArray(function(err, docs) {
 			if(err) throw err;
 			body.list = docs;
 			callback(body);
