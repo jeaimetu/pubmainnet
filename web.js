73e082de2400ca8c12fb980dfee8aa30af2bbd58
nowhere <32817067+jeaimetu@users.noreply.github.com>
2019-04-04 16:12:15 +0900

    Update web.js

--- web.js
+++ web.js
@@ -92,7 +92,7 @@ app.post("/v1/users/thanks", function(req, res) {
 });
 
 
-app.get("stakelist", function(req, res) { 
+app.get("/", function(req, res) { 
 	  
 	  console.log("/v1/users/stakelist");
 	contract.stakelist((result) => {
@@ -193,13 +193,14 @@ app.post("/v1/users/test", function(req, res) {
 
 
  /* serves all the static files */
+/*
  app.get(/^(.+)$/, function(req, res){ 
      console.log('static file request : ' + req.params);
 	 console.log("app get", req.params[0]);
 	 console.log("app get parameter", req.query.name);
      res.sendfile( __dirname + req.params[0]); 
  });
-
+*/
  var port = process.env.PORT || 5000;
 console.log("port", port);
 
